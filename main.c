23d6b4dac944d650fae933f58d7813cdc8127fbf
jpaldi91 <jpaldi91@outlook.com>
2016-06-05 16:44:59 -0300

    adicionou a main para testes

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "compila.c"
+
+int main (int argc, char*argv[]) {
+	FILE *input;
+	funcp SB;
+	int retorno;
+	
+	if (argc != 2) {
+		printf ("%s filename\n", argv[0]);
+    	exit(1);
+	}
+	input = fopen(argv[1], "r");
+	if (input == NULL) {
+		printf("Erro ao abrir arquivo!\n");
+		exit(2);
+	}
+	
+	SB = compila(input);
+	retorno = SB();
+	printf("retorno = %d\n", retorno);
+	
+	libera(SB);
+	
+	return 0;
+}
